59e99c3fb531ee05d981dea511e9524efe55cfb8
thelouisgram <hadesanoye01@gmail.com>
2023-02-24 14:07:43 +0100

    ES6 & selectMode

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const root = document.documentElement;
-const mode = document.querySelector('#check')
+export default selectMode;
 
-mode.addEventListener('click', function(){
-    if (root.classList.contains('dark')){
-    root.classList.remove('dark')
-    }else{
-  root.classList.add('dark')  
+
+const root = document.documentElement;
+export const mode = document.querySelector('#check');
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+function selectMode(){
+    if (prefersDarkScheme.matches){
+      root.classList.toggle("light");
+    } else {
+      root.classList.toggle("dark");
     }
-})
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+import selectMode from "./app.js"
+import {mode} from "./app.js"
+
+
+mode.addEventListener('click', selectMode)
